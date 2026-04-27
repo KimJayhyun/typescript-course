15bf8ce361ce4a5d47d2d5173113b6c4a280ee9f
KimJayhyun <dnlwjem000@gmail.com>
2026-04-27 19:11:37 +0900

    init demo project

---
+++ calculator.ts
@@ -0,0 +1,15 @@
+// data:
+// initial amount
+// annual contribution
+// expected return
+// duration
+
+function calculateInvestment(data) {} // => result[]
+
+function printResults(results) {
+  // print the result data
+}
+
+const result = calculateInvestment(...)
+
+printResults(result);

e211d015c524d54c8a2330732c2f027a2673f4ac
KimJayhyun <79262624+KimJayhyun@users.noreply.github.com>
2026-04-28 21:28:28 +0900

    Section 5: Next-generation JavaScript & TypeScript (#4)
    * install section code
    
    * let vs var
    
    * Spread Operator
    
    * Rest Parameters
    
    * Array & Object Destructuring

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,32 @@
-import fs from "node:fs";
+const hobbies = ["Sports", "Cooking"];
+const activeHobbies = ["Hiking"];
 
-let appName = "test-app";
+activeHobbies.push(...hobbies);
+
+const personHobbies = ["Hiking", ...hobbies];
+
+const person = {
+  name: "Max",
+  age: 30,
+};
+
+// hard copy of the person object
+const copiedPerson = { ...person };
+
+// =========== //
+// Rest Parameters //
+
+const add = (...numbers: number[]) => {
+  return numbers.reduce((curResult, curValue) => {
+    return curResult + curValue;
+  }, 0);
+};
+
+const addedNumbers = add(5, 10, 15, -15, -10);
+
+// =========== //
+//  Array & Object Destructuring //
+
+const [hobby1, hobby2, ...remainingHobbies] = hobbies;
+const { name: maxName, age } = person;
+const { name: personName, age: personAge } = person;
